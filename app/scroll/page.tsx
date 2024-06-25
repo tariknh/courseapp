e63b24fbf2f5b346b8b3f313a048d2e0bbce2617
tariknh <hajibtarik@gmail.com>
2024-06-25 17:11:48 +0200

    Testing

---
+++ app/scroll/page.tsx
@@ -0,0 +1,53 @@
+"use client"
+import { MotionValue, useInView, useScroll, useTransform } from 'framer-motion'
+import React, { useEffect, useRef } from 'react' 
+import {motion} from "framer-motion"
+
+const ScrollElement = (progress: any,left:any) => {
+    //const x = useTransform(progress, [0,1], [0,300])
+    return(
+        <motion.div
+        style={{left,x:progress}}
+        className='relative overflow-hidden whitespace-nowrap'
+        >
+
+            <Slider/>
+            <Slider/>
+            <Slider/>
+        </motion.div>
+    )
+}
+
+const Slider = () => {
+   
+    return(
+        <h1 className='text-6xl whitespace-nowrap'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem?  </h1>
+
+    )
+}
+
+const Page = () => {
+    const container = useRef(null)
+
+    const inView = useInView(container)
+    const {scrollYProgress} = useScroll({
+        target: container,
+        offset: ["start end", "end start"]
+    })
+   
+    
+  return (
+    <section className='bg-white'>
+        <div className='h-screen'>
+
+        </div>
+        <div ref={container} className='h-screen relative flex mb-40'>
+            <ScrollElement left="-20%" progress={scrollYProgress}/>
+            <ScrollElement left="0%" progress={scrollYProgress}/>
+            <ScrollElement left="20%" progress={scrollYProgress}/>
+        </div>
+    </section>
+  )
+}
+
+export default Page
